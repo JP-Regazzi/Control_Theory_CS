f826a24e38e967258664626ac2d4d211ad63b454
Ricardo Chiquetto do Lago <ricardodolago@gmail.com>
2023-10-19 13:02:01 +0200

    Add files via upload

---
+++ TP1/Matlab/Implantation_Cmd_LQGI_quarc_win64/Implantation_Cmd_LQGI_types.h
@@ -0,0 +1,30 @@
+/*
+ * Implantation_Cmd_LQGI_types.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "Implantation_Cmd_LQGI".
+ *
+ * Model version              : 9.2
+ * Simulink Coder version : 9.8 (R2022b) 13-May-2022
+ * C source code generated on : Thu Oct 19 12:50:03 2023
+ *
+ * Target selection: quarc_win64.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_Implantation_Cmd_LQGI_types_h_
+#define RTW_HEADER_Implantation_Cmd_LQGI_types_h_
+
+/* Parameters (default storage) */
+typedef struct P_Implantation_Cmd_LQGI_T_ P_Implantation_Cmd_LQGI_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_Implantation_Cmd_LQGI_T RT_MODEL_Implantation_Cmd_LQG_T;
+
+#endif                           /* RTW_HEADER_Implantation_Cmd_LQGI_types_h_ */
